723e3937953c1afe01abe4766a77e42ea50a418a
aly <pinoofhalyn86@gmail.com>
2026-06-20 01:23:11 +0800

    connecting database

---
+++ Human_Resource_Management_System/DatabaseConnection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace infomanagement
+{
+    public static class DatabaseConnection
+    {
+        
+        private static readonly string server = "localhost";
+        private static readonly string database = "infomanagement";
+        private static readonly string uid = "root";       
+        private static readonly string password = "stormxoreo@0924";      
+
+        
+        private static readonly string connectionString = $"server={server};database={database};uid={uid};password={password};Convert Zero Datetime=True;";
+
+       
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+        public static bool TestConnection()
+        {
+            using (MySqlConnection conn = GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Database Connection Failed:\n" + ex.Message, "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+        }
+    }
+}

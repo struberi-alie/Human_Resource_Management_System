7b13b47560c2723189423ec0042135fdd0e90eec
Riri1106 <reinawilhelminamalicse06@gmail.com>
2026-06-22 00:08:34 +0800

    Create main dashboard design

---
+++ Human_Resource_Management_System/MainDashboard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Human_Resource_Management_System
+{
+    public partial class MainDashboard : UserControl
+    {
+        public MainDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void pnltotalemployee_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lstbxRecentActivity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
